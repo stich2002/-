26a98a7af5879a39bab89761a50a4aacc3c068eb
Elena <elena15112002@gmail.com>
2022-10-03 15:56:29 +0500

    save2

---
+++ program/Program.cs
@@ -0,0 +1,11 @@
+﻿// Массив сортируется по количеству символов элемента (<=3)
+
+Console.WriteLine("Введите количество элементов в массиве");
+int n=Convert.ToInt32(Console.ReadLine());
+int[]array = new int[n];
+
+for (int i = 0; i < n; i++)
+    {
+        
+        if (array[i].length<=3)
+    }
